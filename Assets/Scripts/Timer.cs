977d6152b6c2bd9eed8575f746ec35add605c77d
SmolinIV <Smolin1992@bk.ru>
2024-03-29 15:15:00 +0500

    Done

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+
+    private Coroutine _timeCounter;
+
+    private float _timerValueInSeconds;
+    private bool _isTimerWork;
+
+    private void Start()
+    {
+        _isTimerWork = false;
+        _timerValueInSeconds = 0;
+    }
+
+    private void OnDisable()
+    {
+        if (_timeCounter != null)
+            StopCoroutine(_timeCounter);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown((int)MouseButton.Left))
+        {
+            _isTimerWork = !_isTimerWork;
+
+            if (_isTimerWork)
+                _timeCounter = StartCoroutine(CountSeconds());
+            else
+                StopCoroutine(_timeCounter);
+        }
+    }
+
+    private IEnumerator CountSeconds()
+    {
+        float delayInSeconds = 0.5f;
+
+        WaitForSeconds delay = new WaitForSeconds(delayInSeconds);
+
+        yield return delay;
+
+        while (_isTimerWork)
+        {
+            _timerValueInSeconds += delayInSeconds;
+            UpdateTimeText();
+
+            yield return delay;
+        }
+    }
+
+    private void UpdateTimeText()
+    {
+        _text.text = _timerValueInSeconds.ToString("0.0");
+    }
+}
+
